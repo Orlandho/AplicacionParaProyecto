b609fb6484b8b46b9dfd83b32bdbca48fc9cd01b
Orlandho <72486369+Orlandho@users.noreply.github.com>
2025-05-15 17:20:55 -0500

    feat(full proyect) first commit

---
+++ src/Seguridad/Usuario.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Seguridad;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author walri
+ */
+public class Usuario {
+     private String usuario;
+    private String contraseña;
+    private String rol;
+    private LocalDate fechaUltimoCambio;
+    
+    //Constructor
+
+    public Usuario(String usuario, String contraseña, String rol, LocalDate fechaUltimoCambio) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+        this.fechaUltimoCambio = fechaUltimoCambio;
+    }
+    
+    //Metodos get y set
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public LocalDate getFechaUltimoCambio() {
+        return fechaUltimoCambio;
+    }
+
+    public void setFechaUltimoCambio(LocalDate fechaUltimoCambio) {
+        this.fechaUltimoCambio = fechaUltimoCambio;
+    }
+
+    // Validación para cambio de contraseña obligatorio
+    public boolean debeCambiarContraseña() {
+        return fechaUltimoCambio.plusDays(60).isBefore(LocalDate.now());
+    }
+}
