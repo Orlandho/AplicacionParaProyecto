f13cc4468e5ca67cb65b8f6b9bc70943c56bdb0a
Orlandho <72486369+Orlandho@users.noreply.github.com>
2025-05-21 12:03:58 -0500

    fix(Persona.java) now has get set methods and constructor

--- src/Clases/Persona.java
+++ src/Clases/Persona.java
@@ -11,4 +11,44 @@ package Clases;
 public abstract class Persona {
     
     protected String nombre, apellido, genero, direccion;
+
+    public Persona(String nombre, String apellido, String genero, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
 }
