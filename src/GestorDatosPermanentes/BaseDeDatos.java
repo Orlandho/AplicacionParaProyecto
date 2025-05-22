fef26252a4bf7c43f9328602398254ad553f3400
Orlando Dorival <72486369+Orlandho@users.noreply.github.com>
2025-05-21 19:43:48 -0500

    doc(BaseDeDatos.java) test comment removed (#18)
    test comment gone

--- src/GestorDatosPermanentes/BaseDeDatos.java
+++ src/GestorDatosPermanentes/BaseDeDatos.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 public class BaseDeDatos {
 
-    //TEST COMMENT BY ORLANDO TESTING RULESETS
     private Map<String, Usuario> usuarios;
     public static final int USUARIO_CONTRA_INCORRECTOS = 0, USUARIO_BLOQUEADO = 1, DEBE_CAMBIAR_CONTRASEÑA = 2, PUEDE_INGRESAR = 3;
 
