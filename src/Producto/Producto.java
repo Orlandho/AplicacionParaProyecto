8cea81ebdafbe0893963290edba3eb4da7d3a2d5
Luis Alberto Moreyra Asencios <cualquiercosaqueyosepa@gmail.com>
2025-06-11 13:07:27 -0500

    Feat/producto (#67)
    * Feat/max length and exit control (#64)
    
    * feat(VerificadorUsuario.java FrmMenuDinamico.java) added muyLargo method to check Strings length in FrmMenuDinamico.java
    
    * feat(FrmLogin.java FrmMenuDinamico.java) when trying to exit or log out the form shows a confirmation message. Also formatted all .java files
    
    * feat: AtualizarUsuario y EliminarUsuario
    
    * Descripción del cambio en baseDeDatos.db
    
    * class: Creación de la clase Producto
    
    * fix: Cambios en la clase Producto
    
    * feat: implementación metodo buscarProducto
    
    * feat: implementación metodo obtenerProductos
    
    * feat: implementación metodo crearProducto
    
    * feat: implementación metodo eliminarProducto
    
    * feat: implementación metodo actualizarProducto
    
    * Actualización de archivos
    
    * fix: Cambio de importe
    
    * fix: Cambio de lugar de la clase
    
    * Relocalizando Producto.java y actualizado baseDeDatos.db
    
    ---------
    
    Co-authored-by: Orlando Dorival <72486369+Orlandho@users.noreply.github.com>

---
+++ src/Producto/Producto.java
@@ -0,0 +1,60 @@
+
+package Producto;
+
+public class Producto {
+    
+    // Atributos
+    private String tipoDocumento;
+    private String producto;
+    private double precioCompra;
+    private int cantidad;
+    private String stock;
+
+    public Producto(String tipoDocumento, String producto, double precioCompra, int cantidad, String stock) {
+        this.tipoDocumento = tipoDocumento;
+        this.producto = producto;
+        this.precioCompra = precioCompra;
+        this.cantidad = cantidad;
+        this.stock = stock;
+    }
+    
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+}
