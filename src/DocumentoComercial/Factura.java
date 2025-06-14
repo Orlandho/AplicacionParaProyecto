78a4512b7fc1b23f3a6adb6f4069c3cc1a19cbe3
Orlandho <72486369+Orlandho@users.noreply.github.com>
2025-06-14 03:05:10 -0500

    conflictFix(FrmMenuDinamico.java) fixed conflict

---
+++ src/DocumentoComercial/Factura.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DocumentoComercial;
+
+import java.util.ArrayList;
+import Producto.Producto;
+
+/**
+ *
+ * @author ORLANDO
+ */
+public class Factura {
+
+    private int ID;
+    private long serie;
+    private long numero;
+    private String proveedor;
+    private String moneda;
+    private String responsable;
+    private ArrayList<Producto> listaProductos;
+
+    public Factura(int ID, long serie, long numero, String proveedor, String moneda, String responsable, ArrayList<Producto> listaProductos) {
+        this.ID = ID;
+        this.serie = serie;
+        this.numero = numero;
+        this.proveedor = proveedor;
+        this.moneda = moneda;
+        this.responsable = responsable;
+        this.listaProductos = listaProductos;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public long getSerie() {
+        return serie;
+    }
+
+    public void setSerie(long serie) {
+        this.serie = serie;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public double getTotal() {
+        return 0;
+    }
+
+}
