02c0492d0764399ecbf028022d1c1fa4d8982250
Orlandho <72486369+Orlandho@users.noreply.github.com>
2025-07-05 00:43:14 -0500

    feat(FrmMenuDinamico.java) form got the ability to change language

--- src/MenuDinamico/FrmMenuDinamico.java
+++ src/MenuDinamico/FrmMenuDinamico.java
@@ -88,6 +88,14 @@ public class FrmMenuDinamico extends javax.swing.JFrame {
         tempListComprEmitid = new ArrayList<>();
         tempListVenProf= new ArrayList<>();
         tblPadre = null;
+        leerLenguaje();
+    }
+    
+    private void leerLenguaje(){
+        String[] configuracion=baseDeDatos.obtenerAjustes();
+        if(configuracion[0].equals("ingles")){
+            Ajuste.Ajustes.cambiarAIngles(null, this);
+        }
     }
 
     public void modificarSegunRol(Usuario usuario) {
@@ -409,6 +417,7 @@ public class FrmMenuDinamico extends javax.swing.JFrame {
         btnGuardacambiosAjustes = new javax.swing.JButton();
         lblApectoAjustes = new javax.swing.JLabel();
         lblLogoAjustes = new javax.swing.JLabel();
+        cbLenguaje = new javax.swing.JComboBox<>();
         pnlRegVenProform = new javax.swing.JPanel();
         lblsubtemaProformaRegVen = new javax.swing.JLabel();
         txtFechaProformaRegVen = new javax.swing.JTextField();
@@ -2183,6 +2192,15 @@ public class FrmMenuDinamico extends javax.swing.JFrame {
         pnlAjustes.add(lblLogoAjustes);
         lblLogoAjustes.setBounds(340, 100, 70, 30);
 
+        cbLenguaje.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Español", "Ingles" }));
+        cbLenguaje.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cbLenguajeItemStateChanged(evt);
+            }
+        });
+        pnlAjustes.add(cbLenguaje);
+        cbLenguaje.setBounds(60, 370, 160, 30);
+
         tpnMostrar.addTab("pnlAjustes", pnlAjustes);
 
         pnlRegVenProform.setBackground(new java.awt.Color(211, 251, 155));
@@ -3796,6 +3814,24 @@ public class FrmMenuDinamico extends javax.swing.JFrame {
         tpnMostrar.setSelectedIndex(iPnlVenProforma);
     }//GEN-LAST:event_jmOTROSRegVenActionPerformed
 
+    private void cbLenguajeItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbLenguajeItemStateChanged
+        int selec=cbLenguaje.getSelectedIndex();
+        if(selec==-1){
+            return;
+        }
+        switch(selec){
+            case 0:
+                baseDeDatos.actualizarAjustes("español","claro");
+                Ajuste.Ajustes.cambiarAEspanol(null, this);
+                break;
+            case 1:
+                baseDeDatos.actualizarAjustes("ingles","claro");
+                Ajuste.Ajustes.cambiarAIngles(null, this);
+                break;
+        }
+        
+    }//GEN-LAST:event_cbLenguajeItemStateChanged
+
 
     /**
      * @param args the command line arguments
@@ -3891,6 +3927,7 @@ public class FrmMenuDinamico extends javax.swing.JFrame {
     private javax.swing.JLabel btnregistrodeusuario;
     private javax.swing.JLabel btnregistrodeventas;
     private javax.swing.JLabel btnreportes;
+    private javax.swing.JComboBox<String> cbLenguaje;
     private javax.swing.JComboBox<String> cbTipoAspectoAjustes;
     private javax.swing.JComboBox<String> cbTipoDeDineroBoleta;
     private javax.swing.JComboBox<String> cbTipoDeDineroBoletaRegVen;
